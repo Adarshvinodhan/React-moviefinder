12b9347ff5ea2daa532ee202f1c20e52c88b4bfc
adarsh <adarshvinodhan@gmail.com>
2024-11-24 16:45:36 +0530

    added animations

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,9 +6,12 @@ function SearchBar({handleInput,result}) {
 <div className="text-center p-6 bg-gray-900 min-h-screen flex flex-col items-center justify-center relative">
   {/* Header */}
   <h1 className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500 mb-6 animate-fadeInUp">
-  Movie Finder
-</h1>
+    Movie Finder
+  </h1>
 
+  <h3 className='font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500 mb-6 animate-slideInLeft'>
+    Search you're fav Movies & add them to you're Fav's <span className='text-red-400 animate-heart'>❤️</span>
+  </h3>
 
   {/* Input and Button Container */}
   <div className="flex gap-4 items-center mb-8 w-full max-w-md">
@@ -53,10 +56,6 @@ function SearchBar({handleInput,result}) {
     <span className='font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500 animate-fadeInUp'>Your Fav's</span>
   </Link>
 </div>
-
-
-
-  )
-}
+)}
 
 export default SearchBar
